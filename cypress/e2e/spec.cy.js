eb62a771af20d9faae44963ec1007b7cee6e8f46
Hk-mungai <hosea@thejumba.com>
2023-04-28 12:49:30 +0300

    minor fix

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('Loads succesfully', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.url().should('eq','http://localhost:3000/')
+    cy.get('#root > div > header > a').should('have.text','Learn React').and('be.visible').and('exist')
+    
+    
+  })
+})
