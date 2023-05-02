390d0bf30ef8ae07c0212ab09f93179f17fc484e
Hk-mungai <hosea@thejumba.com>
2023-05-02 13:42:51 +0300

    fix

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,22 +1,10 @@
 /// <reference types="cypress" />
-describe('frontend',function () { 
-  beforeEach(() => {  
-      cy.visit('https://testui.thejumba.com/', {
-          auth: {
-          username: 'jumba-test-user',
-          password: '$jumbaDC@2023#',
-          }
-      });  
-  })            
-  it('Loads & reloads succesfully', function() {
+describe('Loads succesfully', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.url().should('eq','http://localhost:3000/')
+    cy.get('#root > div > header > a').should('have.text','Learn React').and('be.visible').and('exist')
 
-    
-      cy.title().should("eq","Jumba")
-      cy.reload();
-      cy.title().should("eq","Jumba")   
-      
-      cy.get('.jumba-logo--01-1').should("be.visible").should("exist").click({force: true})
-      cy.url().should("eq","https://testui.thejumba.com/")
-     
+
   })
 })
