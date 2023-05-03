5e1e3012b40cd2f5dc31c55304e6c36d1304c3de
Hk-mungai <122884835+Hk-mungai@users.noreply.github.com>
2023-05-03 16:29:54 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,7 @@ describe('Loads succesfully', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
     cy.url().should('eq','http://localhost:3000/')
+     cy.get('#root > div > header > a').should('have.text','Learn React').and('be.visible').and('exist')
    
 
   })
